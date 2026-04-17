26491c018d7afbcbc0b352f9ff0d87e4a5f44aa3
helpguideteam <madhusudang2g@gmail.com>
2026-04-17 17:35:00 +0530

    navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en">
-      <body className="bg-gray-100">
+      <body className="bg-gray-200">
         <UserProvider>
           <Navbar />
           {children}
